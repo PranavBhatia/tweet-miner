af45ca97f9929779788d5eba80298d67ccec671d
PranavBhatia <bhatiapranav1996@gmail.com>
2018-08-08 15:50:39 -0400

    Singleton Implemented

--- app/services/TwitterObject.java
+++ app/services/TwitterObject.java
@@ -7,35 +7,38 @@ import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- * 
  * @author Pranav Bhatia
- *
  */
 public class TwitterObject {
     /**
-	 * Get a twitter instance after authentication
-	 * @return a Twitter instance
-	 */
-	public static boolean testCase = false;
+     * Get a twitter instance after authentication
+     *
+     * @return a Twitter instance
+     */
+    public static boolean testCase = false;
     public static int emotion = 1;
-	public static Twitter getInstance() {
+    private static Twitter singletonTwitterInstance = null;
+
+    public static Twitter getInstance() {
 
         if (!testCase) {
-            ConfigurationBuilder cb = new ConfigurationBuilder();
-            cb.setDebugEnabled(true).setOAuthConsumerKey("uvZYPifCuMDmDLhGzryVaH9sA")
-                    .setOAuthConsumerSecret("G01gHXhUerHAevCjhR0U1iOlm5VNmxC5cGRnldvHscVFcMfkvQ")
-                    .setOAuthAccessToken("969353476450017280-r7gPo5QAT8svSDiCBoB0jSKu1f3Oa8P")
-                    .setOAuthAccessTokenSecret("TmapsealCXor82pige4FwRZ16tqKollMbBb6AieVcKVrJ");
-            TwitterFactory tf = new TwitterFactory(cb.build());
-            Twitter twitterInstance = tf.getInstance();
-            return twitterInstance;
-        }else{
-            if(emotion == 1)
+            if (singletonTwitterInstance == null) {
+                ConfigurationBuilder cb = new ConfigurationBuilder();
+                cb.setDebugEnabled(true).setOAuthConsumerKey("uvZYPifCuMDmDLhGzryVaH9sA")
+                        .setOAuthConsumerSecret("G01gHXhUerHAevCjhR0U1iOlm5VNmxC5cGRnldvHscVFcMfkvQ")
+                        .setOAuthAccessToken("969353476450017280-r7gPo5QAT8svSDiCBoB0jSKu1f3Oa8P")
+                        .setOAuthAccessTokenSecret("TmapsealCXor82pige4FwRZ16tqKollMbBb6AieVcKVrJ");
+                TwitterFactory tf = new TwitterFactory(cb.build());
+                singletonTwitterInstance = tf.getInstance();
+            }
+            return singletonTwitterInstance;
+        } else {
+            if (emotion == 1)
                 return new PseudoTwitterHappy();
             else if (emotion == -1)
                 return new PseudoTwitterSad();
             else
                 return new PseudoTwitterNeutral();
         }
-	}
+    }
 }
